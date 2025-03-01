4b88000ee39d5e1922b45e1b8f356916f2a513ea
osmangonirabbi13 <osmangoniyou12@gmail.com>
2025-03-01 16:07:03 +0600

    add

--- script/date.js
+++ script/date.js
@@ -1,3 +1,19 @@
 const d = new Date();
 document.getElementById("time").innerHTML = d.toDateString();
 
+function myTime(){
+    let now = new Date();
+    let hours = now.getHours().toString();
+    let minutes = now.getMinutes().toString();
+    let seconds = now.getSeconds().toString();
+    hours = hours % 12 || 12;
+    let amPm = hours >= 12 ? "AM" : "PM";
+
+    let time = `${hours}:${minutes}:${seconds} ${amPm}`;
+  
+    // console.log( typeof times)
+  
+    const timeat = time.innerHTML = time;
+    return timeat;
+}
+
--- script/script.js
+++ script/script.js
@@ -3,7 +3,7 @@ document.getElementById('btn1').addEventListener('click' , function(event){
     event.preventDefault
     alert('board updated successfully')
     const task = parseInt(document.getElementById('task-count').innerText);
-    console.log(task)
+    // console.log(task)
     const Completed = parseInt(document.getElementById('task-completed').innerText);
     document.getElementById('task-completed').innerText= Completed + 1;
     // console.log(typeof Completed)
@@ -21,7 +21,7 @@ document.getElementById('btn1').addEventListener('click' , function(event){
   
     p.setAttribute("style" , "background-color: #F4F7FF; padding: 15px; border-radius: 5px; margin-bottom: 15px;");
   
-    p.innerText =  'you have completed the tack Fix Mobile Button Issue';
+    p.innerText =  ` You have completed the tack Fix Mobile Button Issue at ${myTime()} `;
   
     history.appendChild(p);
   
@@ -38,7 +38,100 @@ document.getElementById('btn2').addEventListener('click' , function(event){
     event.preventDefault
     alert('board updated successfully')
     const task = parseInt(document.getElementById('task-count').innerText);
-    console.log(task)
+    // console.log(task)
+    const Completed = parseInt(document.getElementById('task-completed').innerText);
+    document.getElementById('task-completed').innerText= Completed + 1;
+    // console.log(typeof Completed)
+    if (task > 0) {
+        document.getElementById('task-count').innerText = task - 1;
+    }else{
+        document.getElementById('task-count').innerText = 0;
+    }
+
+    const history = document.getElementById("task-history");
+
+    const p = document.createElement("div");   
+  
+    p.setAttribute("style" , "background-color: #F4F7FF; padding: 15px; border-radius: 8px; margin-bottom: 15px;");
+  
+    p.innerText =  `You have completed the task Add Dark Mode ${myTime()}`;
+  
+    history.appendChild(p);
+  
+   const btn2= document.getElementById('btn2')
+   btn2.setAttribute("disabled" , "true");
+  
+})
+
+// button 03 task completed
+
+document.getElementById('btn3').addEventListener('click' , function(event){
+    event.preventDefault
+    alert('board updated successfully')
+    const task = parseInt(document.getElementById('task-count').innerText);
+    // console.log(task)
+    const Completed = parseInt(document.getElementById('task-completed').innerText);
+    document.getElementById('task-completed').innerText= Completed + 1;
+    // console.log(typeof Completed)
+    if (task > 0) {
+        document.getElementById('task-count').innerText = task - 1;
+    }else{
+        document.getElementById('task-count').innerText = 0;
+    }
+
+    const history = document.getElementById("task-history");
+
+    const p = document.createElement("div"); 
+  
+    p.setAttribute("style" , "background-color: #F4F7FF; padding: 15px; border-radius: 8px; margin-bottom: 15px;");
+  
+    p.innerText =  `You have completed the task Optimize Home page ${myTime()}`;
+  
+    history.appendChild(p);
+  
+   const btn3= document.getElementById('btn3')
+   btn3.setAttribute("disabled" , "true");
+  
+})
+
+// button 04 task completed
+
+document.getElementById('btn4').addEventListener('click' , function(event){
+    event.preventDefault
+    alert('board updated successfully')
+    const task = parseInt(document.getElementById('task-count').innerText);
+    // console.log(task)
+    const Completed = parseInt(document.getElementById('task-completed').innerText);
+    document.getElementById('task-completed').innerText= Completed + 1;
+    // console.log(typeof Completed)
+    if (task > 0) {
+        document.getElementById('task-count').innerText = task - 1;
+    }else{
+        document.getElementById('task-count').innerText = 0;
+    }
+
+    const history = document.getElementById("task-history");
+
+    const p = document.createElement("div");
+  
+    p.setAttribute("style" , "background-color: #F4F7FF; padding: 15px; border-radius: 8px; margin-bottom: 15px;");
+  
+    p.innerText =  `You have completed the task Add new emoji 🤲 ${myTime()}`;
+  
+    history.appendChild(p);
+  
+   const btn4= document.getElementById('btn4')
+   btn4.setAttribute("disabled" , "true");
+  
+})
+
+// button 05 task completed
+
+document.getElementById('btn5').addEventListener('click' , function(event){
+    event.preventDefault
+    alert('board updated successfully')
+    const task = parseInt(document.getElementById('task-count').innerText);
+    // console.log(task)
     const Completed = parseInt(document.getElementById('task-completed').innerText);
     document.getElementById('task-completed').innerText= Completed + 1;
     // console.log(typeof Completed)
@@ -54,15 +147,59 @@ document.getElementById('btn2').addEventListener('click' , function(event){
   
     
   
-    p.setAttribute("style" , "background-color: #F4F7FF; padding: 15px; border-radius: 5px; margin-bottom: 15px;");
+    p.setAttribute("style" , "background-color: #F4F7FF; padding: 15px; border-radius: 8px; margin-bottom: 15px;");
   
-    p.innerText =  'you have completed the tack Fix Mobile Button Issue';
+    p.innerText =  `You have completed the task Integrate OpenAI API ${myTime()}`;
   
     history.appendChild(p);
   
-   const btn2= document.getElementById('btn2')
-   btn2.setAttribute("disabled" , "true");
+   const btn5= document.getElementById('btn5')
+   btn5.setAttribute("disabled" , "true");
   
+})
+
+// button 06 task completed
+
+document.getElementById('btn6').addEventListener('click' , function(event){
+    event.preventDefault
+    alert('board updated successfully')
+    alert('congrates!!! You have completed all the current task')
+    const task = parseInt(document.getElementById('task-count').innerText);
+    // console.log(task)
+    const Completed = parseInt(document.getElementById('task-completed').innerText);
+    document.getElementById('task-completed').innerText= Completed + 1;
+    // console.log(typeof Completed)
+    if (task > 0) {
+        document.getElementById('task-count').innerText = task - 1;
+    }else{
+        document.getElementById('task-count').innerText = 0;
+    }
+
+    const history = document.getElementById("task-history");
+
+    const p = document.createElement("div");
   
+    
   
+    p.setAttribute("style" , "background-color: #F4F7FF; padding: 15px; border-radius: 8px; margin-bottom: 15px;");
+  
+    p.innerText =  `You have completed the task Improve Job searching ${myTime()}`;
+  
+    history.appendChild(p);
+  
+   const btn6= document.getElementById('btn6')
+   btn6.setAttribute("disabled" , "true");
+  
+})
+
+const history = document.getElementById('clear-history').addEventListener('click' , function(event){
+    event.preventDefault
+    document.getElementById('task-history').innerHTML = '';
+})
+
+// blog click
+
+document.getElementById('blogClick').addEventListener('click' , function(event){
+    event.preventDefault
+    location.href='blog.html'
 })
